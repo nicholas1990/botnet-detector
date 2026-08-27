cbbf3b5b5e86e6fe825724926407f6b1b9d4302d
nicholas <nicholas.angelucci@gmail.com>
2026-08-27 11:32:13 +0200

    Implement compute_work_weight per spec formula

--- src/analysis/work_weight.py
+++ src/analysis/work_weight.py
@@ -2,4 +2,6 @@
 
 
 def compute_work_weight(syn_sent, fin_sent, rst_received, total_tcp_packets):
-    raise NotImplementedError
+    if total_tcp_packets == 0:
+        return 0.0
+    return (syn_sent + fin_sent + rst_received) / total_tcp_packets
